cffff61c3e21bf2d0062903cc5fd2db5969cc630
placidowang <placidow@gmail.com>
2020-04-07 10:59:15 -0500

    Done.

--- src/components/GalaxySNote7.js
+++ src/components/GalaxySNote7.js
@@ -18,16 +18,40 @@ export default class GalaxySNote7 extends React.Component {
     }, false)
   }
 
+  // sleep = (milliseconds) => {
+  //   const date = Date.now();
+  //   let currentDate = null;
+  //   do {
+  //     currentDate = Date.now();
+  //   } while (currentDate - date < milliseconds);
+  // }
+
   throwAFit = () => {
+    // if (this.state.panicked) return
+    // setTimeout(()=>{this.props.alterEnvironment("inhospitable")}, 2000)
+    // this.setState({ panicked: true },
+    //   ()=>{
+    //     this.exclaim()
+    //     setTimeout(()=>{this.relax()}, 4000)
+    //   })
+
+    this.props.alterEnvironment("inhospitable")
+    setTimeout(()=>{this.relax()}, 2000)
   }
 
   relax = () => {
+    this.props.alterEnvironment('docile')
+    this.setState({ panicked: false })
+    console.log('chill')
   }
 
   exclaim = () => {
     if (this.state.panicked) return
+    this.setState({panicked: true})
+    console.log('Panicked!')
     this.exclaimAudio.play()
     this.squeelAudio.play()
+    // event listener in constructor invokes throwAFit after exclaim audio finishes playing
   }
 
   panic = () => (<img id="galaxy-exclamation" className="exclamation" src={exclamation} alt="" />)
